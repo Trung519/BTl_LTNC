8adfa06055d2ff8ac13012bce3e1d03623d37948
summer0404 <157554348+summer0404@users.noreply.github.com>
2024-04-03 16:21:16 +0700

    minor fixes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import LoginForm from "./components/LoginForrm/LoginForm";
 import SignUpForm from "./components/SignupForm/SignUpForm";
-import EquipmentsManage from "./components/Equipments_Manage/EquipmentsManage";
+import EquipmentsManage from "./components/Equipments_Products_Manage/EquipmentsManage";
 import "./App.css";
 
 function App() {
