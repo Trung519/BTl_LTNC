9e4fec809cafc4e0b9d988ae22b8ed21d3bd0485
Nguyen Nhat Phap <nguyennhatphapbp@gmail.com>
2024-04-03 10:23:32 +0700

    added firebase to manage_Equipment

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import LoginForm from './components/LoginForrm/LoginForm';
-import SignUpForm from './components/SignupForm/SignUpForm';
-import { useState } from 'react';
-import EquipmentsManage from './components/Equipments_Products_Manage/EquipmentsManage';
-import Modal from './components/Equipments_Products_Manage/Modal';
-import './App.css';
+import LoginForm from "./components/LoginForrm/LoginForm";
+import SignUpForm from "./components/SignupForm/SignUpForm";
+import { useState } from "react";
+import EquipmentsManage from "./components/Equipments_Products_Manage/EquipmentsManage";
+import Modal from "./components/Equipments_Products_Manage/Modal";
+import "./App.css";
 
 function App() {
-  
   return (
     <div>
-       
       <EquipmentsManage />
     </div>
   );
