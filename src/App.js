adfa9892c1c6bd9eb647c21ce2940dd13cdb5668
Nguyen Nhat Phap <nguyennhatphapbp@gmail.com>
2024-04-04 11:37:35 +0700

    change st

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import LoginForm from "./components/LoginForrm/LoginForm";
 import SignUpForm from "./components/SignupForm/SignUpForm";
 import EquipmentsManage from "./Pages/EquipmentsManage/EquipmentsManage";
+import Home from "./Pages/Home";
+
 import "./App.css";
+// import { Home } from "@mui/icons-material";
 
 function App() {
   return (
     <div>
-      <EquipmentsManage />
+      <Home />
     </div>
   );
 }
