b51580edb1b7c525ad82df18cfe0377cee18b525
HoangLinhDo <linh.dohoang@hcmut.edu.vn>
2024-04-09 00:11:19 +0700

    header cho Truc

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -7,43 +7,52 @@ function Footer() {
     return (
         <>
             <div className={cx('container-fluid', 'footer')}>
-                <div className={cx('container', 'footer-info')}>
-                    <div className={cx('footer-info-left')}>
-                        <h2>BỆNH VIỆN ĐA KHOA TÂM ĐỨC</h2>
-                        <div className={cx('to-flex', 'left-address')}>
-                            <i class="fas fa-map-marker-alt"></i>
-                            <p>Địa chỉ: KTX khu A, ĐHQP-TPHCM</p>
+                <div className={cx('container')}>
+                    <div className={cx('row')}>
+                        <div className={cx('footer-info-left', 'col-md-4')}>
+                            <h2>BỆNH VIỆN ĐA KHOA TÂM ĐỨC</h2>
+                            <div className={cx('footer-slogan')}>
+                                <div>Sức khỏe của bạn</div>
+                                <div>Ưu tiên của chúng tôi</div>
+                            </div>
                         </div>
-                        <div className={cx('to-flex', 'left-email')}>
-                            <i class="far fa-envelope"></i>
-                            <p>Email: abc@hcmut.edu.vn</p>
+                        <div className={cx('footer-info-center', 'col-md-4')}>
+                            <div className={cx('center-title')}>
+                                <h4>Thông tin</h4>
+                            </div>
+                            <div className={cx('to-flex', 'left-address')}>
+                            <svg class="svg-inline--fa fa-map-marker-alt fa-w-12" aria-hidden="true" data-prefix="fas" data-icon="map-marker-alt" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" data-fa-i2svg=""><path fill="currentColor" d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0zM192 272c44.183 0 80-35.817 80-80s-35.817-80-80-80-80 35.817-80 80 35.817 80 80 80z"></path></svg>
+                                <p>Địa chỉ: KTX khu A, ĐHQP-TPHCM.</p>
+                            </div>
+                            <div className={cx('to-flex', 'left-email')}>
+                            <svg class="svg-inline--fa fa-envelope fa-w-16" aria-hidden="true" data-prefix="far" data-icon="envelope" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M464 64H48C21.49 64 0 85.49 0 112v288c0 26.51 21.49 48 48 48h416c26.51 0 48-21.49 48-48V112c0-26.51-21.49-48-48-48zm0 48v40.805c-22.422 18.259-58.168 46.651-134.587 106.49-16.841 13.247-50.201 45.072-73.413 44.701-23.208.375-56.579-31.459-73.413-44.701C106.18 199.465 70.425 171.067 48 152.805V112h416zM48 400V214.398c22.914 18.251 55.409 43.862 104.938 82.646 21.857 17.205 60.134 55.186 103.062 54.955 42.717.231 80.509-37.199 103.053-54.947 49.528-38.783 82.032-64.401 104.947-82.653V400H48z"></path></svg>
+                                <p>Email: abc@hcmut.edu.vn</p>
+                            </div>
+                            <div className={cx('to-flex', 'left-hotline')}>
+                            <svg class="svg-inline--fa fa-mobile-alt fa-w-10" aria-hidden="true" data-prefix="fas" data-icon="mobile-alt" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" data-fa-i2svg=""><path fill="currentColor" d="M272 0H48C21.5 0 0 21.5 0 48v416c0 26.5 21.5 48 48 48h224c26.5 0 48-21.5 48-48V48c0-26.5-21.5-48-48-48zM160 480c-17.7 0-32-14.3-32-32s14.3-32 32-32 32 14.3 32 32-14.3 32-32 32zm112-108c0 6.6-5.4 12-12 12H60c-6.6 0-12-5.4-12-12V60c0-6.6 5.4-12 12-12h200c6.6 0 12 5.4 12 12v312z"></path></svg>
+                                <p>Hotline: 0987.654.321</p>
+                            </div>
+                            <div className={cx('to-flex', 'left-website')}>
+                            <svg class="svg-inline--fa fa-globe-asia fa-w-16" aria-hidden="true" data-prefix="fas" data-icon="globe-asia" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512" data-fa-i2svg=""><path fill="currentColor" d="M248 8C111.03 8 0 119.03 0 256s111.03 248 248 248 248-111.03 248-248S384.97 8 248 8zm-11.34 240.23c-2.89 4.82-8.1 7.77-13.72 7.77h-.31c-4.24 0-8.31 1.69-11.31 4.69l-5.66 5.66c-3.12 3.12-3.12 8.19 0 11.31l5.66 5.66c3 3 4.69 7.07 4.69 11.31V304c0 8.84-7.16 16-16 16h-6.11c-6.06 0-11.6-3.42-14.31-8.85l-22.62-45.23c-2.44-4.88-8.95-5.94-12.81-2.08l-19.47 19.46c-3 3-7.07 4.69-11.31 4.69H50.81C49.12 277.55 48 266.92 48 256c0-110.28 89.72-200 200-200 21.51 0 42.2 3.51 61.63 9.82l-50.16 38.53c-5.11 3.41-4.63 11.06.86 13.81l10.83 5.41c5.42 2.71 8.84 8.25 8.84 14.31V216c0 4.42-3.58 8-8 8h-3.06c-3.03 0-5.8-1.71-7.15-4.42-1.56-3.12-5.96-3.29-7.76-.3l-17.37 28.95zM408 358.43c0 4.24-1.69 8.31-4.69 11.31l-9.57 9.57c-3 3-7.07 4.69-11.31 4.69h-15.16c-4.24 0-8.31-1.69-11.31-4.69l-13.01-13.01a26.767 26.767 0 0 0-25.42-7.04l-21.27 5.32c-1.27.32-2.57.48-3.88.48h-10.34c-4.24 0-8.31-1.69-11.31-4.69l-11.91-11.91a8.008 8.008 0 0 1-2.34-5.66v-10.2c0-3.27 1.99-6.21 5.03-7.43l39.34-15.74c1.98-.79 3.86-1.82 5.59-3.05l23.71-16.89a7.978 7.978 0 0 1 4.64-1.48h12.09c3.23 0 6.15 1.94 7.39 4.93l5.35 12.85a4 4 0 0 0 3.69 2.46h3.8c1.78 0 3.35-1.18 3.84-2.88l4.2-14.47c.5-1.71 2.06-2.88 3.84-2.88h6.06c2.21 0 4 1.79 4 4v12.93c0 2.12.84 4.16 2.34 5.66l11.91 11.91c3 3 4.69 7.07 4.69 11.31v24.6z"></path></svg>
+                                <p>Website: https://mybk.hcmut.edu.vn/my/index.action</p>
+                            </div>
                         </div>
-                        <div className={cx('to-flex', 'left-hotline')}>
-                            <i class="fas fa-mobile-alt"></i>
-                            <p>Hotline: 0987.654.321</p>
-                        </div>
-                        <div className={cx('to-flex', 'left-website')}>
-                            <i class="fas fa-globe-asia"></i>
-                            <p>Website: https://mybk.hcmut.edu.vn/my/index.action</p>
+                        <div className={cx('footer-info-right', 'col-md-4')}>
+                            <h4>Theo dõi chúng tôi</h4>
+                            <div className={cx('contact-items')}>
+                                <div>
+                                    <svg class="svg-inline--fa fa-facebook fa-w-14" aria-hidden="true" data-prefix="fab" data-icon="facebook" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg=""><path fill="currentColor" d="M448 56.7v398.5c0 13.7-11.1 24.7-24.7 24.7H309.1V306.5h58.2l8.7-67.6h-67v-43.2c0-19.6 5.4-32.9 33.5-32.9h35.8v-60.5c-6.2-.8-27.4-2.7-52.2-2.7-51.6 0-87 31.5-87 89.4v49.9h-58.4v67.6h58.4V480H24.7C11.1 480 0 468.9 0 455.3V56.7C0 43.1 11.1 32 24.7 32h398.5c13.7 0 24.8 11.1 24.8 24.7z"></path></svg>
+                                </div>
+                                <div>
+                                    <svg class="svg-inline--fa fa-youtube fa-w-18" aria-hidden="true" data-prefix="fab" data-icon="youtube" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" data-fa-i2svg=""><path fill="currentColor" d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z"></path></svg>
+                                </div>
+                                <div>
+                                    <svg class="svg-inline--fa fa-instagram fa-w-14" aria-hidden="true" data-prefix="fab" data-icon="instagram" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg=""><path fill="currentColor" d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"></path></svg>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div className={cx('footer-info-right')}>
-                        <h4>Liên hệ với chúng tôi</h4>
-                        <div className={cx('contact-items')}>
-                            <div>
-                                <i class="fab fa-facebook"></i>
-                            </div>
-                            <div>
-                                <i class="fab fa-youtube"></i>
-                            </div>
-                            <div>
-                                <i class="fab fa-instagram"></i>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className={cx('container', 'footer-dev')}>
-                    <p>Phát triển bới BTL LTNC</p>
+
                 </div>
             </div>
         </>
--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,6 +1,5 @@
 import classNames from "classnames/bind";
 import styles from "./Home.module.scss";
-import Header from "../../Components/Header";
 import Footer from "../../Components/Footer";
 
 const cx = classNames.bind(styles);
@@ -12,25 +11,9 @@ function Home() {
                 <div className={cx('home')}>
                     <div className={cx('content')}>
                         <p>CHĂM SÓC SỨC KHỎE</p>
-                        <h1>DẪN ĐẦU VỀ</h1>
-                        <h1>CHUYÊN MÔN Y KHOA</h1>
-                        <div className={cx('characters')}>
-                            <div className={cx('character')}>
-                                <p>Tận tâm</p>
-                                <i className={cx("fas fa-notes-medical")}></i>
-                            </div>
-                            <div className={cx('character')}>
-                                <p>Nhanh chóng</p>
-                                <i className={cx("fas fa-stopwatch")}></i>
-                            </div>
-                            <div className={cx('character')}>
-                                <p> Hiệu quả</p>
-                                <i className={cx("character3","fas fa-file-medical-alt")}></i>
-                            </div>
-                            <div className={cx('character')}>
-                                <p>Chất lượng</p>
-                                <i className={cx("fas fa-heart")}></i>
-                            </div>
+                        <div>
+                            <h1>DẪN ĐẦU VỀ</h1>
+                            <h1>CHUYÊN MÔN Y KHOA</h1>
                         </div>
                     </div>
                 </div>
