341770a20f85bf7f608918cae60d9bc96d52afc3
TaQuangTruc <truc.taquang23904@hcmut.edu.vn>
2024-05-09 15:25:16 +0700

    i or y?

--- src/Pages/PatientRecord/components/HistoryRow.js
+++ src/Pages/PatientRecord/components/HistoryRow.js
@@ -61,7 +61,7 @@ const HistoryRow = (props) => {
         {/* ********Xác nhận lấy thuốc */}
         {getMed ? (
           `Được xác nhận bởi ${pharmacist.name}`
-        ) : user.typeEmp === "Dược sỹ" || user.typeEmp === "Quản trị" ? (
+        ) : user.typeEmp === "Dược sĩ" || user.typeEmp === "Quản trị" ? (
           <Button
             variant="outlined"
             onClick={() => {
